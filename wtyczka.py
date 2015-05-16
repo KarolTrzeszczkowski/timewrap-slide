7db4a8d54745e2e25686e71fdc60480724580c50
Karol Trzeszczkowski <karol.trzeszczkowski@gmail.com>
2015-05-16 14:10:51 +0200

    komendy

--- wtyczka.py
+++ wtyczka.py
@@ -1,4 +1,13 @@
-no witam
+import gimpfu
+gimp.image_list()
 
+images=gimp.image_list()
+img=images[0]
+img.layers
+layers=img.layers
+len(img.layers)
+for k in range(10):
+    layers[k].translate(-50+k*10,0)
 
-eloszka dud kolejnee
+ 
+
