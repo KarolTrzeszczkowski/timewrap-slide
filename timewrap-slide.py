7635659e1b2401f06c04a0ca7b920ef6d2e43352
Karol Trzeszczkowski <karol.trzeszczkowski@gmail.com>
2015-05-17 15:11:21 +0200

    z regulowalną prędkością

--- timewrap-slide.py
+++ timewrap-slide.py
@@ -4,11 +4,11 @@
 
 from gimpfu import *
 
-def timewrap_slide(img, cos, cos2) :
+def timewrap_slide(img, drawable, v) :
     layers=img.layers
     n=len(img.layers)
     for k in range(n):
-        layers[k].translate(-50+k*10,0)
+        layers[k].translate(-50+k*v,0)
 
 register(
     "python_fu_Timewrap_slide",
@@ -20,7 +20,7 @@ register(
     "<Image>/Filters/Test/Timewrap Slide",
     "*",
     [
-        (PF_IMAGE, "img", "Input image", None)
+        (PF_Int, "v", "Prędkość", 10)
     ],
     [],
     timewrap_slide)
