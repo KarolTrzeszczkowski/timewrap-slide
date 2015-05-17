383e8f1c2242635246355f7ae657fe327a9b6391
Karol Trzeszczkowski <karol.trzeszczkowski@gmail.com>
2015-05-17 14:04:12 +0200

    pierwsza na prawdę dzialajaca wersja

--- timewrap-slide.py
+++ timewrap-slide.py
@@ -4,7 +4,7 @@
 
 from gimpfu import *
 
-def timewrap_slide(img, drawable) :
+def timewrap_slide(img, cos, cos2) :
     layers=img.layers
     n=len(img.layers)
     for k in range(n):
@@ -17,11 +17,10 @@ register(
     "Licho-Karol Trzeszczkowski",
     "Licho",
     "2015",
-    "<Image>/Filters/Test/Hello world",
+    "<Image>/Filters/Test/Timewrap Slide",
     "*",
     [
-        (PF_IMAGE, "img", "Input image", None),
-        (PF_DRAWABLE, "drawable", "Input drawable", None)
+        (PF_IMAGE, "img", "Input image", None)
     ],
     [],
     timewrap_slide)
